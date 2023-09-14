f243b50b76f3934923d0b8441d1c340143da30da
ATHARVA J FULARI <82925329+atharvafulari@users.noreply.github.com>
2023-09-14 12:35:11 +0530

    Test case for parameterized constructor.
    Changes are made in code to pass the test case.
    Test case passed successfully.

--- Chandrayaan3_main.java
+++ Chandrayaan3_main.java
@@ -6,5 +6,16 @@ package com.incubyte.Chandrayaan3_tdd;
  */
 public class Chandrayaan3_main 
 {
-    
+	private int x;
+	private int y;
+	private int z;
+	private String direction;
+
+//	parametirsed constructor
+	public Chandrayaan3_main(int x, int y, int z, String direction) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.direction = direction;
+	}
 }
