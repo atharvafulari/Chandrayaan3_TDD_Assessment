8836d2cb926f3aa2b09c4e92d52ab75619f70541
ATHARVA J FULARI <82925329+atharvafulari@users.noreply.github.com>
2023-09-14 15:40:37 +0530

    Code Changed to pass the test cases.
    Test case passed for moveforward method in all direction.

--- Chandrayaan3_main.java
+++ Chandrayaan3_main.java
@@ -41,6 +41,14 @@ public class Chandrayaan3_main
 			y++;
 		} else if ("S".equals(direction)) {
 			y--;
-		} 
+		} else if ("E".equals(direction)) {
+			x++;
+		} else if ("W".equals(direction)) {
+			x--;
+		} else if ("U".equals(direction)) {
+			z++;
+		} else if ("D".equals(direction)) {
+			z--;
+		}
 	}
 }
