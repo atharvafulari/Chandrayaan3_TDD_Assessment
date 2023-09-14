97335389b3afc21dd19dbf38e9e234b5258e5b1b
ATHARVA J FULARI <82925329+atharvafulari@users.noreply.github.com>
2023-09-14 12:30:56 +0530

    Test case created for parametrized constructor.
    Test case failed.
    Test case checks weather class is created or not and also checks for parametrized constructor.

---
+++ Chandrayaan3_main.java
@@ -0,0 +1,10 @@
+package com.incubyte.Chandrayaan3_tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class Chandrayaan3_main 
+{
+    
+}
---
+++ Chandrayaan3_test.java
@@ -0,0 +1,17 @@
+package com.incubyte.Chandrayaan3_tdd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class Chandrayaan3_test 
+{
+   
+    @Test
+    public void constructor_test()
+    {
+    	Chandrayaan3_main obj=new Chandrayaan3_main(0,0,0, "N");
+        
+    }
+}
